b6ede65c434bf00d5351b34c0f42766133b0b007
Joskii <wt911122@163.com>
2023-12-07 10:36:32 +0800

    :wrench: 增加部署脚本

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Setting up the public directory
+app.use(express.static('public', {
+  setHeaders: (res) => {
+    res.set('Cross-Origin-Opener-Policy', 'same-origin');
+    res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+  }
+}));
+
+app.listen(3000, () => console.log('listening on port 3000!'));
